39530b0c025194724e154a200ddf84a1da6bfb85
Martin Crnjakovic <icytheoctopus@gmail.com>
2019-11-09 16:41:44 +0100

    Created initial migrations | Added .idea to .gitignore

---
+++ database/migrations/2019_11_09_152915_create_authors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('firstname');
+            $table->text('lastname');
+            $table->text('city');
+            $table->integer('zip');
+            $table->string('address');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
